19e6bafcab46a4d1f8cad977394a63b736918c32
Manu343726 <Manu343726@gmail.com>
2015-06-06 13:52:21 +0200

    algorithm working. Let's go performant

--- graph.hpp
+++ graph.hpp
@@ -6,7 +6,9 @@
 #include <vector>
 #include <stdexcept>
 #include <cassert>
-#include <ostream>
+#include <iostream>
+#include <chrono>
+#include <random>
 
 #include <manu343726/range/v3/all.hpp>
 #include "utils.hpp"
@@ -370,5 +372,48 @@ public:
     METHOD_FROM(operator(), _matrix)
     METHOD_FROM(at, _matrix)
 };
+
+template<typename Node, bool node_debug_output = false, bool edge_debug_output = false>
+auto random_graph(std::size_t nodes, float density)
+{
+    std::chrono::high_resolution_clock::time_point begin;
+    std::mt19937 prng{ std::random_device{}() };
+    std::uniform_int_distribution<std::size_t> dist{0, nodes-1};
+
+    graph<Node> result;
+    result.reserve(nodes);
+
+    for(std::size_t i = 0; i < nodes; ++i)
+    {
+        if(node_debug_output)
+        {
+            std::cout << "Adding node (" << i << ")... ";
+            begin = std::chrono::high_resolution_clock::now();
+        }
+        result.add_node();
+        if(node_debug_output)
+        {
+            auto end = std::chrono::high_resolution_clock::now();
+            auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+            std::cout <<  elapsed.count() << "ms\n";
+        }
+    }
+
+    for(std::size_t i = 0; i < density; ++i)
+    {
+        if(edge_debug_output)
+            std::cout << "Generating edges (Pass " << i << ")\n";
+
+        for(std::size_t j = 0; j < nodes; ++j)
+        {
+            std::size_t k = dist(prng);
+
+            if(k != j)
+                result(j,k) = true;
+        }
+    }
+
+    return std::move(result);
+}
 
 #endif /* GRAPH_HPP */
--- main.cpp
+++ main.cpp
@@ -7,144 +7,162 @@
 #include <string>
 #include <random>
 #include <chrono>
+#include <numeric>
+#include <queue>
 
-struct node
+struct GraphNode
 {
     int color = -1;
 };
 
-template<typename Node, bool node_debug_output = false, bool edge_debug_output = false>
-auto random_graph(std::size_t nodes, float density)
+struct TreeNode
 {
-    std::chrono::high_resolution_clock::time_point begin;
-    std::mt19937 prng{ std::random_device{}() };
-    std::uniform_int_distribution<std::size_t> dist{0, nodes-1};
+    std::vector<int> solution; // n colors, one per node
+    std::size_t optimistic = 0;
+    std::size_t pesimistic = 0;
+    int last_color = 0;
+    std::size_t k = 0;
 
-    graph<Node> result;
-    result.reserve(nodes);
+    TreeNode(const std::vector<int>& colors, std::size_t optimistic_ = 0, std::size_t pesimistic_ = 0, std::size_t real_cost_ = 0) :
+        solution{colors},
+        optimistic{optimistic_},
+        pesimistic{pesimistic_},
+        last_color{real_cost_}
+    {}
 
-    for(std::size_t i = 0; i < nodes; ++i)
+    TreeNode(std::size_t nodes_count, std::size_t optimistic_ = 0, std::size_t pesimistic_ = 0, std::size_t real_cost_ = 0) :
+        solution(nodes_count, -1),
+        optimistic{optimistic_},
+        pesimistic{pesimistic_},
+        last_color{real_cost_}
+    {}
+
+    template<typename Node>
+    int operator[](const Node& node) const
     {
-        if(node_debug_output)
+        return solution[node.id()];
+    }
+
+    template<typename Node>
+    int& operator[](const Node& node)
+    {
+        return solution[node.id()];
+    }
+
+    int operator[](std::size_t i) const
+    {
+        return solution[i];
+    }
+
+    int& operator[](std::size_t i)
+    {
+        return solution[i];
+    }
+
+    friend bool operator<(const TreeNode& lhs, const TreeNode& rhs)
+    {
+        return lhs.optimistic > rhs.optimistic;
+    }
+};
+
+template<typename Node, typename Optimistic, typename Pesimistic>
+std::vector<int> colorize(const graph<Node>& graph, Optimistic optimistic, Pesimistic pesimistic)
+{
+    TreeNode root{graph.nodes_count()};
+    root.last_color = 0;
+    root.k = 0;
+    root[root.k] = 0;
+    root.pesimistic = pesimistic(root);
+    root.optimistic = optimistic(root);
+
+    std::priority_queue<TreeNode> queue;
+    queue.push(root);
+
+    std::size_t best_cost = graph.nodes_count();
+    std::vector<int> best_solution = root.solution;
+
+    while(!queue.empty() && queue.top().optimistic < best_cost)
+    {
+        TreeNode x = std::move(queue.top()); queue.pop();
+
+        auto neighbors = [&](std::size_t node)
         {
-            std::cout << "Adding node (" << i << ")... ";
-            begin = std::chrono::high_resolution_clock::now();
-        }
-        result.add_node();
-        if(node_debug_output)
+            return graph.neighbors(node) | ranges::view::remove_if([&](const auto& neighbor)
+            {
+                return x[neighbor] < 0; //discard colored nodes
+            });
+        };
+
+        auto colors = [&](std::size_t node)
         {
-            auto end = std::chrono::high_resolution_clock::now();
-            auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-            std::cout <<  elapsed.count() << "ms\n";
+            return neighbors(node) | ranges::view::transform([&](const auto& neighbor)
+            {
+                return x[neighbor];
+            });
+        } ;
+
+        for(int color = 0; color <= x.last_color+1; ++color)
+        {
+            auto neighbors_colors = colors(x.k+1);
+
+            if(ranges::find(neighbors_colors, color) == std::end(neighbors_colors))
+            {
+                TreeNode y = x;
+                y.k += 1;
+                y[y.k] = color;
+                y.last_color = std::max(color, x.last_color);
+                y.optimistic = optimistic(y);
+                y.pesimistic = pesimistic(y);
+
+                if(y.optimistic < best_cost)
+                {
+                    if(y.k == graph.nodes_count() - 1)
+                    {
+                        best_cost = y.last_color;
+                        best_solution = std::move(y.solution);
+                    }
+                    else
+                    {
+                        queue.push(std::move(y));
+                        best_cost = std::min(best_cost, pesimistic(y));
+                    }
+                }
+            }
         }
     }
 
-    for(std::size_t i = 0; i < density; ++i)
-    {
-        if(edge_debug_output)
-            std::cout << "Generating edges (Pass " << i << ")\n";
-
-        for(std::size_t j = 0; j < nodes; ++j)
-        {
-            std::size_t k = dist(prng);
-
-            if(k != j)
-                result(j,k) = true;
-        }
-    }
-
-    return std::move(result);
-}
-
-template<typename Node>
-auto non_colored_neighbors(const graph<Node>& g, std::size_t node)
-{
-    return g.neighbors(node) | ranges::view::remove_if([](auto n)
-    {
-       return n.color >= 0;
-    });
-}
-
-template<typename Node>
-auto non_colored_neighbors(graph<Node>& g, std::size_t node)
-{
-    return g.neighbors(node) | ranges::view::remove_if([](auto n)
-    {
-       return n.color >= 0;
-    });
-}
-
-template<typename Range>
-int first_color_available(Range nodes)
-{
-    int color = 0;
-
-    for(auto node : ranges::view::bounded(nodes))
-        color = std::max(color, node.color);
-
-    return color;
-}
-
-template<typename Node>
-int first_color_available(const graph<Node>& g, std::size_t node)
-{
-    return first_color_available(g.neighbors(node));
-}
-
-template<typename Range>
-std::size_t length(Range r)
-{
-    return std::end(r) - std::begin(r);
-}
-
-template<typename Node>
-void color_graph_naive(graph<Node>& g, std::size_t node)
-{
-    auto& current = g(node);
-
-    if(current.color < 0)
-        current.color = first_color_available(g, node);
-
-    auto neighbors = non_colored_neighbors(g, node);
-
-    for (auto &neighbor : neighbors)
-    {
-        neighbor.color = current.color + 1;
-        color_graph_naive(g, neighbor);
-    }
-}
-
-template<typename Node>
-bool connected(const graph<Node>& g)
-{
-    return ranges::all_of(g.nodes() | ranges::view::bounded, [&](const auto& node)
-    {
-       return !ranges::empty(g.neighbors(node.id()));
-    });
+    return best_solution;
 }
 
 int main(int argn, const char* argv[])
 {
     std::size_t nodes = argn > 1 ? std::atoi(argv[1]) : 10;
 
-    auto graph = random_graph<node, true, true>(nodes,std::log10(nodes));
-
-    if(!connected(graph))
-        std::cout << "WARNING: Non-connected graph" << std::endl;
-
-    color_graph_naive(graph, 0);
+    auto graph = random_graph<GraphNode, true, true>(nodes,std::log10(nodes));
 
     int max_color = -1;
 
+    auto optimistic = [](const TreeNode& node)
+    {
+        return node.last_color;
+    };
+
+    auto pesimistic = [&](const TreeNode& node)
+    {
+        return graph.nodes_count();
+    };
+
+    auto colors = colorize(graph, optimistic, pesimistic);
+
     for(std::size_t i = 0; i < nodes; ++i)
     {
-        std::cout << "(" << i << ") color: " << graph(i).color
+        std::cout << "(" << i << ") color: " << colors[i]
                   << " neighbors: ";
 
-        max_color = std::max(graph(i).color, max_color);
+        max_color = std::max(colors[i], max_color);
 
         for(auto neighbor : graph.neighbors(i))
-            std::cout << "(" << neighbor.id() << ") ";
+            std::cout << "(" << neighbor.id() << "{c:" << colors[neighbor.id()] << "}) ";
 
         std::cout << std::endl;
     }
